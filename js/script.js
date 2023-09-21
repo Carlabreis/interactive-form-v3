12d7dfbb31711f687ee767aebd45cb8b5d3f63ca
Carla Borges Reis <carlareis.dev.com>
2023-09-21 10:27:14 -0700

    tshirt info section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const userName = document.querySelector("#name");
 const otherJobRole = document.querySelector("#other-job-role");
 const jobRoleSelect = document.querySelector("#title");
+const colorSelect = document.querySelector("#color");
+const designSelect = document.querySelector("#design");
 
 userName.focus();
 otherJobRole.style.display = 'none';
@@ -11,4 +13,31 @@ jobRoleSelect.addEventListener("change", (e) => {
     } else {
         otherJobRole.style.display = 'none';
     }
-})
+});
+
+//disable color select element
+colorSelect.disabled = true;
+
+designSelect.addEventListener("change", (e) => {
+    const heartJsOptions = colorSelect.querySelectorAll('[data-theme="heart js"]');
+    const jsPunsOptions = colorSelect.querySelectorAll('[data-theme="js puns"]');
+    
+    colorSelect.disabled = false;
+
+    if (e.target.value === "js puns") {
+        for (let i = 0; i < heartJsOptions.length; i++) {
+            heartJsOptions[i].setAttribute("hidden", "");
+        }
+        for (let i = 0; i < jsPunsOptions.length; i++) {
+            jsPunsOptions[i].removeAttribute("hidden", "");
+        }
+    } else {
+        for (let i = 0; i < heartJsOptions.length; i++) {
+            heartJsOptions[i].removeAttribute("hidden", "");
+        }
+        for (let i = 0; i < jsPunsOptions.length; i++) {
+            jsPunsOptions[i].setAttribute("hidden", "");
+        }
+    }
+});
+
