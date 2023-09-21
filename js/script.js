22d4504395b60bb9d0d038a8cf0289c6eac8d4fa
Carla Borges Reis <carlareis.dev.com>
2023-09-21 09:01:47 -0700

    add event listener to job role select and display/hide other input field

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 const userName = document.querySelector("#name");
+const otherJobRole = document.querySelector("#other-job-role");
+const jobRoleSelect = document.querySelector("#title");
+
 userName.focus();
+otherJobRole.style.display = 'none';
+
+jobRoleSelect.addEventListener("change", (e) => {
+    if (e.target.value === "other") {
+        otherJobRole.style.display = 'block';
+    } else {
+        otherJobRole.style.display = 'none';
+    }
+})
